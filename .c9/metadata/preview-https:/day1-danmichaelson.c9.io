353e26b1419219438d8ad29f180bc0a69d9f2ed1
Dan Michaelson <dan@linkedbyair.net>
2015-02-12 17:59:52 +0000

    Code in advance of Feb 12 lab. Basic CRUD (well just Index and Create) with MongoDB and Mongoose.

--- .c9/metadata/preview-https:/day1-danmichaelson.c9.io
+++
@@ -1,1 +0,0 @@
-{"filter":false,"title":"[P] https://day1-danmichaelson.c9.io","undoManager":{"mark":0,"position":-1,"stack":[]},"preview":{"path":"https://day1-danmichaelson.c9.io","previewer":"preview.browser"},"timestamp":1423514534797,"hash":"67452301efcdab8998badcfe10325476c3d2e1f0"}
--- routes/index.js
+++ routes/index.js
@@ -2,77 +2,86 @@ var express = require('express');
 var router = express.Router();
 module.exports = router;
 
+// MongoDB is a JavaScript-oriented database.
+// http://docs.mongodb.org/manual/core/crud-introduction/
+// You need to start MongoDB before running your app by typing ./mongod at the Terminal.
+//   But you only need to do that once per workspace; it should run forever after that.
 
-// This code only runs when we first start the server
+// Mongoose makes it easy to access MongoDB using "models".
+// http://mongoosejs.com
 
-// Initialize a counter with the value 1. This will count the total visitors.
-var counter = 1;
+// Use Mongoose to connect to the MongoDB database. We'll call our
+// database "networks". It will be created automatically if it doesn't already exist.
 
-// Store three colors in an array (a list). This acts as a kind of database.
-var colors = ['red', 'green', 'blue'];
-
-// Initialize a counter for our colors. This keeps track of which color to show.
-var color_num = 0;
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://' + process.env.IP + '/networks');
 
 
-// ===== HOME PAGE WITH TEMPLATE =====
+var Person = mongoose.model('Person', {
+  name: {type: String, required: true},
+  age: {type: Number, required: true, min: 0, max: 200}
+});
 
-router.get('/', function(request, response) {
-  
-  // This code runs every time a request for "/" comes in
-  
-  var time = new Date();
 
-  // Render the "views/home.hbs" template file, and pass an object that the
-  // template can use to show some dynamic values ("hour", "counter", etc.)
-  // The "layout" property is a special one, and means to surround the "home"
-  // template with the "layout.hbs" template.
-  
-  // Note that layout.hbs loads the /public/stylesheets/style.css stylesheet.
-  // That is a static file, so you can't insert dynamic values into that. But
-  // you can always use inline style="..." attributes and insert dynamic values
-  // into those, or even put an inline <style>...</style> stylesheet at the top of
-  // home.hbs and insert dynamic values into that.
-  
-  // Other static files such as images can also go into the public folder, and
-  // can then be referenced from your HTML templates (see the <img> example in about.hbs).
-  
-  // .hbs means "Handlebars template", more about it here: http://handlebarsjs.com/
-  //   Ignore the documentation about "helpers" (that's an advanced topic).
-  //   Basically {{hour}} will insert the value of the hour property you pass below,
-  //   and that's pretty much all you need to know.
-  //   {{{hour}}} will do the same, the only difference is if your value is text 
-  //   that contains a "<" or ">", {{hour}} will display those characters to the 
-  //   end user as  < and > (technically it will change them to &lt; and &gt;), 
-  //   whereas {{{hour}}} will treat them as an HTML tag.
-  
-  response.render('home', { 
-      hour: time.getHours(), 
-      minute: time.getMinutes(),
-      counter: counter,
-      color: colors[color_num], // This will be "red" when color_num is 0, etc.
-      layout: 'layout'
+
+// HOME PAGE
+
+router.get('/', function(request, response, toss) {
+
+  Person.find(function (err, people) {
+
+    if (err) return toss(err);
+    
+    response.locals.people = people;
+    response.locals.layout = 'layout';
+
+    response.render('home');
+
   });
-
-  // Increase the total visitors counter
-  counter += 1;
-  
-  // Increase the colors counter
-  color_num += 1;
-  // When the colors counter is >= 3, go back to 0. This way colors[color_num]
-  // cycles between red, green, and blue.
-  if (color_num >= colors.length) {
-    color_num = 0;
-  }
   
 });
 
 
-// ====== ABOUT PAGE WITH TEMPLATE =====
+
+// NEW PAGE
+
+router.get('/new', function(request, response) {
+
+  response.locals.layout = 'layout';
+  response.render('new');
+  
+});
+
+
+
+// CREATE PAGE
+
+router.get('/create', function(request, response, toss) {
+  response.locals.layout = 'layout';
+
+  var person = new Person({
+    name: request.query.name,
+    age: request.query.age  
+  });
+  
+  person.save(function(err) {
+
+    if (err) return toss(err);
+
+    response.locals.person = person;
+    response.render('create');
+
+  });
+  
+});
+
+
+
+// ABOUT PAGE
 
 router.get('/about', function(request, response) {
-  // This code runs every time a request for "/about" comes in
-  // Rendr the "views/about.hbs" template file.
-  response.render('about', { layout: 'layout'} );
-})
 
+  response.locals.layout = 'layout';
+  response.render('about');
+  
+});
