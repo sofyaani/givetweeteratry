18fd87295bccd0aace4ecec47a53f029e4a612c2
sofyaani <maziyar.pahlevan@gmail.com>
2015-04-16 18:30:09 +0000

    Description of my changes

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.set("view options", { layout: false });
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var moment = require('moment');
-var feeds = require('../feeds.js');
 
 module.exports = router;
 
@@ -38,14 +37,14 @@ mongoose.connect('mongodb://' + process.env.IP + '/networks');
 // =   DEFINE OUR DATA MODELS
 // =
 
-// Define the data structure of a Phrase model
-// It has width, height, top, and left attributes, which are required to be numbers from 0–100
-// And it has a color attribute, which is optionaland is a string (text)
+// Define the data structure of a Message model
+// The number field is from 1-100. It's the number we're drawing.
+// The text field is the text we'll draw around that number.
 // Allowed data types (Number, String, Date...): http://mongoosejs.com/docs/schematypes.html
 
-var Phrase = mongoose.model('Phrase', {
-  preposition: {type: String, required: true},
-  noun: {type: String, required: true},
+var Message = mongoose.model('Message', {
+  text: {type: String, required: true},
+  number: {type: Number, required: true}
 });
 
 
@@ -60,53 +59,25 @@ var Phrase = mongoose.model('Phrase', {
 
 // HOME PAGE
 // /
-// Shows _all_ the phrases
+// Shows _all_ the messages
 
 router.get('/', function(request, response, toss) {
   
   // When the server receives a request for "/", this code runs
 
-  // Find all the Shape records in the database
-  Phrase.find(function(err, phrases) {
+  // Find all the Message records in the database, sorted descending by number
+  Message.find().sort({number: -1}).exec(function(err, messages) {
     // This code will run once the database find is complete.
-    // phrases will contain a list (array) of all the phrases that were found.
+    // messages will contain a list (array) of all the messages that were found.
     // err will contain errors if any.
 
     // If there's an error, tell Express to do its default behavior, which is show the error page.
     if (err) return toss(err);
     
-    // The list of shapes will be passed to the template.
+    // The list of messages will be passed to the template.
     // Any additional variables can be passed in a similar way (response.locals.foo = bar;)
-    response.locals.phrases = phrases;
+    response.locals.messages = messages;
     
-    // Also pass the temperature, wind direction, and next bus arrival.
-    // This can crash if the data hasn't loaded for whatever reason, so toss to Express's error page in that case.
-    try {
-      
-      // Use Moment.js to calculate the next bus arrival time minus the current time,
-      // and display it in english e.g. "in 18 minutes".
-      // http://momentjs.com/docs/#/displaying/from/ (returns difference as an english string e.g. "18 minutes")
-      // http://momentjs.com/docs/#/displaying/difference/ (returns difference in milliseconds)
-      var now = moment();
-      var next_arrival = moment(feeds.bus[0].arrival_at);
-      var arriving_in = next_arrival.from(now);
-      var margin = next_arrival.diff(now) / 1000;
-
-      response.locals.temperature = feeds.weather.main.temp;
-      response.locals.font_size = feeds.weather.main.temp / 2;
-      response.locals.wind_direction = feeds.weather.wind.deg;
-      response.locals.arriving_in = arriving_in;
-      response.locals.margin = margin;
-      response.locals.route = feeds.bus[0].route_id;
-      
-    }
-    catch(err) {
-      return toss(err);
-    }
-    
-    // layout tells template to wrap itself in the "layout" template (located in the "views" folder).
-    response.locals.layout = 'layout';
-
     // Render the "home" template (located in the "views" folder).
     response.render('home');
 
@@ -117,52 +88,70 @@ router.get('/', function(request, response, toss) {
 
 
 
-// SHOW PAGE
-// /show?id=54e2058e85b156d10b064ca0
-// Shows a _single_ phrase
-
-router.get('/show', function(request, response, toss) {
-  
-  // When the server receives a request for "/show", this code runs
-  
-  // Find a Phrase with this id
-  Phrase.findOne({_id: request.query.id}, function(err, phrase) {
-    // This code will run once the database find is complete.
-    // phrase will contain the found phrase.
-    // err will contain errors if any (for example, no such record).
-
-    if (err) return toss(err);
-    
-    response.locals.phrase = phrase;
-    response.locals.layout = 'layout';
-    response.render('show');
-    
-  });
-  
-});
-
-
-
 // NEW PAGE
 // /new
 
 router.get('/new', function(request, response) {
-
+  
   // When the server receives a request for "/new", this code runs
   
-  // Just render a basic HTML page with a form. We don't need to pass any variables.
+  // Figure out the lowest number that has been stored so far
+  Message.findOne().sort({number: 1}).exec(function(err, message) {
+    
+    if (err) return toss(err);
 
-  response.locals.layout = 'layout';
-  response.render('new');
-  
-  // Please see views/new.hbs for additional comments
-  
+    // Pass the next number to the input form, to tell the user
+    var number;
+    if (message) {
+      number = message.number - 1;
+      // TODO: If we're down to zero, do something different
+    }
+    else {
+      number = 100;
+    }
+    if (number < 0) number = 0;
+    if (number > 100) number = 100;
+    
+    var lengths = {
+      0: 115,
+      1: 30,
+      2: 117,
+      3: 113,
+      4: 79,
+      5: 118,
+      6: 134,
+      7: 62,
+      8: 152,
+      9: 134
+    };
+    
+    var maxlength;
+    if (number >= 100) {
+      maxlength = lengths[String(number).substr(0, 1)] + lengths[String(number).substr(1, 1)] + lengths[String(number).substr(2, 1)];
+    }
+    else if (number >= 10) {
+      maxlength = lengths[String(number).substr(0, 1)] + lengths[String(number).substr(1, 1)];
+    }
+    else {
+      maxlength = lengths[number];
+    }
+    
+    console.log("maxlength", maxlength);
+    
+    response.locals.number = number;
+    response.locals.maxlength = maxlength;
+
+    response.render('new');
+
+  });
+
+
 });
 
 
 
 // CREATE PAGE
-// /create?width=25&height=25&top=25&left=25&color=#ff0000
+// /create?text=abc&number=100
 // Normally you get to this page by clicking "Submit" on the /new page, but
 // you could also enter a URL like the above directly into your browser.
 
@@ -172,43 +161,23 @@ router.get('/create', function(request, response, toss) {
   
   response.locals.layout = 'layout';
 
-  // Make a new Phrase in memory, with the parameters that come from the URL 
-  // ?width=25&height=25&top=25&left=25&color=#ff0000
+  // Make a new Message in memory, with the parameters that come from the URL 
   // and store it in the shape variable
-  var phrase = new Phrase({
-    preposition: request.query.preposition,
-    noun: request.query.noun,
+  var message = new Message({
+    text: request.query.text,
+    number: request.query.number,
   });
   
   // Now save it to the database
-  phrase.save(function(err) {
+  message.save(function(err) {
     // This code runs once the database save is complete
 
     // An err here can be due to validations
     if (err) return toss(err);
     
-    // Otherwise render a "thank you" page
-    response.locals.phrase = phrase;
-    response.render('create');
-    
-    // Alternatively we could just do
-    // response.redirect('/');
-    // to send the user straight to the homepage after saving the new shape
+    response.redirect('/');
 
   });
   
 });
 
-
-
-// ABOUT PAGE
-// /about
-
-router.get('/about', function(request, response) {
-
-  // When the server receives a request for "/about", this code runs
-
-  response.locals.layout = 'layout';
-  response.render('about');
-  
-});
